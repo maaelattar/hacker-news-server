a338580237b5fbc8e4db9019f37dd920d58997ef
maaelattar <41055637+maaelattar@users.noreply.github.com>
2020-09-15 03:28:58 +0200

    Add websocket address log message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const { ApolloServer } = require("apollo-server");
-const { GraphQLFileLoader } = require("graphql-tools");
 const { PrismaClient } = require("@prisma/client");
 const { PubSub } = require("graphql-subscriptions");
 const typeDefs = require("./graphql/typeDefs");
@@ -7,6 +6,7 @@ const resolvers = require("./graphql/resolvers");
 
 const prisma = new PrismaClient();
 const pubsub = new PubSub();
+pubsub.ee.setMaxListeners(30); // raise max listeners in event emitter
 
 const server = new ApolloServer({
   typeDefs,
@@ -20,6 +20,7 @@ const server = new ApolloServer({
   },
 });
 
-server.listen().then(({ url }) => {
+server.listen().then(({ url, subscriptionsUrl }) => {
   console.log(`Server ready at: ${url}`);
+  console.log(`Subscriptions ready at: ${subscriptionsUrl}`);
 });
